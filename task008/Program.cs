1276cf2591486b806d15db6dfeb4c3829ffce213
Let0Pda <a.gavrilov70@gmail.com>
2022-08-12 10:11:05 +0300

    Работа над ошибками Домашнее задание Урок 1

--- task008/Program.cs
+++ task008/Program.cs
@@ -1,8 +1,17 @@
-﻿Console.Write("Введите число: ");
-int n = Convert.ToInt32(Console.ReadLine());
-for( int i = 1; i <= n; i++)
+﻿/*
+Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+5 -> 2, 4
+8 -> 2, 4, 6, 8
+*/
 
-    if(i % 2 ==0)
+Console.Clear();
+Console.WriteLine("Задача 8");
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+//for (int i = 1; i <= n; i++)
+for (int i = 2; i <= n; i += 2)
+
+    if (i % 2 == 0)
     {
-        Console.Write( i + " ");
+        Console.Write(i + " ");
     }
