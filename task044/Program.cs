bbfdd88afe1d916f574e856d8229ad860d3cae91
Let0Pda <a.gavrilov70@gmail.com>
2022-08-24 23:35:35 +0300

    добавил задачи 44 и 45

---
+++ task044/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задача 44: Не используя рекурсию, выведите первые N чисел Фибоначчи. Первые два числа Фибоначчи: 0 и 1.
+// Если N = 5 -> 0 1 1 2 3
+// Если N = 3 -> 0 1 1
+// Если N = 7 -> 0 1 1 2 3 5 8
+
+
+Console.Clear();
+Console.Write("Введите первые 'N' чисел Фибоначчи: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+//Console.WriteLine($"\nКоличество чисел Фибоначчи: {N}\n");
+
+int[] GetFibonacci(int N)
+{
+    int firstNumber = 0;
+    int secondNumber = 1;
+    int[] fibonacciNumbers = new int[N];
+    fibonacciNumbers[0] = 0;
+    fibonacciNumbers[1] = 1;
+    for (int i = 1; i < N; i++)
+    {
+        fibonacciNumbers[i] = firstNumber + secondNumber;
+        secondNumber = firstNumber;
+        firstNumber = fibonacciNumbers[i];
+    }
+    return fibonacciNumbers;
+}
+
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write(arr[i] + " ");
+    }
+    //Console.WriteLine();
+}
+
+Console.Write($"\n{N}! = ");
+PrintArray(GetFibonacci(N));
+Console.WriteLine();
---
+++ task045/Program.cs
@@ -0,0 +1,22 @@
+﻿//Задача 45: Напишите программу, которая будет создавать копию заданного массива с помощью поэлементного копирования.
+
+
+Console.Clear();
+Console.Write("Введите кол-во элементов массива: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[N];
+Random rand = new Random();
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = rand.Next(-1000, 1000);
+}
+int[] copyArray = new int[array.Length];
+for (int i = 0; i < array.Length; i++)
+{
+    copyArray[i] = array[i];
+}
+
+Console.Write($"\nИсходный массив из {N} элемнтов ->\t");
+Console.WriteLine('[' + string.Join(", ", array) + ']');
+Console.Write($"\nСкопированный массив из {array.Length} элемнтов ->\t");
+Console.WriteLine('[' + string.Join(", ", copyArray) + ']');
