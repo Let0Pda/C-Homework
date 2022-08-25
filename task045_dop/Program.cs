01a0a1ef4145f9e37cc1326fa9a626e476e473e2
Let0Pda <a.gavrilov70@gmail.com>
2022-08-25 20:12:44 +0300

    28_dop

--- task045_dop/Program.cs
+++ task045_dop/Program.cs
@@ -7,21 +7,12 @@
 Console.Clear();
 Console.Write("Введите число: ");
 int N = Convert.ToInt32(Console.ReadLine());
-double x = 1;
 
-
-int Resalt(int i)
+double Resalt(int i)
 {
     if (i == 1) return 1;
 
     return i * Resalt(i - 1);
 }
 
-// for (int i = 1; i <= N; i++)
-// {
-//     Console.WriteLine($"{i} -> {Resalt(i)}");
-// }
-
-
 Console.WriteLine($"Произведение чисел от 1 до {N} = {Resalt(N)}");
-// Console.WriteLine();
