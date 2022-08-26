990881d8e6dae6e400bac74113ae6e6db35abbfb
Let0Pda <a.gavrilov70@gmail.com>
2022-08-26 20:40:09 +0300

    Домашняя работа к уроку 7

---
+++ task047/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+Console.Clear();
+Console.WriteLine("Задайте двумерный массив m x n");
+Console.Write("\nВведите m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Clear();
+
+Console.WriteLine($"\nДвумерный массив вещественных чисел размера 'm' = {m} на 'n' = {n}:\n");
+double[,] matrix = new double[m, n];
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        matrix[i, j] = new Random().NextDouble() * 20 - 10;
+        Console.Write(Math.Round((matrix[i, j]), 1) + " | ");
+    }
+    Console.WriteLine();
+}
---
+++ task050/Program.cs
@@ -0,0 +1,82 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента 
+//или же указание, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 1,7 -> такого числа в массиве нет
+
+
+Console.Clear();
+int m = 3;
+int n = 4;
+
+Console.Write($"\nСоздан двумерный массив размера 'm = {m}' на 'n = {n}':\n");
+int[,] matrix = new int[m, n];
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        matrix[i, j] = new Random().Next(1, 10);
+        Console.Write(matrix[i, j] + " ");
+    }
+    Console.WriteLine();
+}
+
+Console.Write("\nВведите координаты позиции элемента, разделенных запятой: ");
+
+string positionElement = Convert.ToString(Console.ReadLine());
+RemovingSpaces(positionElement);
+int[] position = Parser(positionElement);
+
+if (position[0] <= m
+    && position[1] <= n
+    && position[0] >= 0
+    && position[1] >= 0)
+{
+    double result = matrix[position[0], position[1]];   // Не очень понятно, как задают позицию, видимые или реальные индексы? 
+                                                        //Возможно и так "double result = matrix[position[0] -1, position[1] -1];"
+    Console.Write($"\nЗначение элемента: {result}\n");
+}
+else Console.Write($"\nТакого элемента в массиве нет.\n");
+
+int[] Parser(string input)
+{
+    int countNum = 1;
+    for (int i = 0; i < input.Length; i++)
+    {
+        if (input[i] == ',' && i <= 2)
+            countNum++;
+    }
+
+    int[] numbers = new int[countNum];
+    int numberIndex = 0;
+    for (int i = 0; i < input.Length; i++)
+    {
+        string subString = String.Empty;
+
+        while (input[i] != ',')
+        {
+            subString += input[i].ToString();
+            if (i >= input.Length - 1)
+                break;
+            i++;
+        }
+        numbers[numberIndex] = Convert.ToInt32(subString);
+        numberIndex++;
+    }
+    return numbers;
+}
+
+string RemovingSpaces(string input)
+{
+    string output = String.Empty;
+    for (int i = 0; i < input.Length; i++)
+    {
+        if (input[i] != ' ')
+        {
+            output += input[i];
+        }
+    }
+    return output;
+}
