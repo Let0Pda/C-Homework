387c393003e93a270f7d4e4b809b9a5198cc9da7
Let0Pda <a.gavrilov70@gmail.com>
2022-08-26 21:07:59 +0300

    задача 47

--- task047/Program.cs
+++ task047/Program.cs
@@ -4,13 +4,13 @@
 // 1 -3,3 8 -9,9
 // 8 7,8 -7,1 9
 
-Console.Clear();
+//Console.Clear();
 Console.WriteLine("Задайте двумерный массив m x n");
 Console.Write("\nВведите m: ");
 int m = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите n: ");
 int n = Convert.ToInt32(Console.ReadLine());
-Console.Clear();
+//Console.Clear();
 
 Console.WriteLine($"\nДвумерный массив вещественных чисел размера 'm' = {m} на 'n' = {n}:\n");
 double[,] matrix = new double[m, n];
@@ -19,7 +19,10 @@ for (int i = 0; i < m; i++)
     for (int j = 0; j < n; j++)
     {
         matrix[i, j] = new Random().NextDouble() * 20 - 10;
-        Console.Write(Math.Round((matrix[i, j]), 1) + " | ");
+        Console.Write(Math.Round((matrix[i, j]), 1) + " |");
+        //Console.Write(String.Format("{0,3}", (Math.Round((matrix[i, j]), 1))) + "|"); // не вижу разницы в выводе
+
+
     }
     Console.WriteLine();
 }
