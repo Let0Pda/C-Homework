1a117fb9ce1cbdf24c042ae1ae23b2b0c4726f3a
Alexey Gavrilov <a.gavrilov70@gmail.com>
2022-09-03 13:44:20 +0300

    ДЗ к уроку 8

--- task061/Program.cs
+++ task061/Program.cs
@@ -1,1 +1,34 @@
 ﻿// Задача 61: Вывести первые N строк треугольника Паскаля. Сделать вывод в виде равнобедренного треугольника
+
+
+
+Console.Clear();
+Console.Write("Введите количество строк треугольника Паскаля: ");
+int indexRow = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("\n");
+int weСonsider(int row)
+{
+    int res = 1;
+    for(int i = 1; i <= row; i++)
+    {
+        res*=i;
+    }
+    return res;
+}
+
+
+for(int i = 0; i < indexRow; i++)
+{
+    for(int j = 0; j <= (indexRow - i); j++)  
+    {
+        Console.Write(" ");
+    }
+    for(int j = 0; j <= i; j++)
+    {
+        Console.Write(" ");
+        Console.Write(weСonsider(i) / (weСonsider(j) * weСonsider(i - j)));
+    }
+    Console.WriteLine();
+}
+
+Console.WriteLine("\n");
--- task062/Program.cs
+++ task062/Program.cs
@@ -5,3 +5,26 @@
 11 16 15 06
 10 09 08 07
 */
+Console.Clear();
+int m = 4;
+int n = 4;
+int[,] array = new int[m, n];
+
+int i = 0;
+int j = 0;
+for(int steps = 1; steps <= m * n; steps++){
+  array[i, j] = steps;
+  if (i <= j + 1 && i + j < m - 1) j++;
+  else if (i < j && i + j >= n - 1) i++;
+  else if (i >= j && i + j > m - 1) j--;
+  else i--;
+}
+
+for (int k = 0; k < n; k++){
+    for (int l = 0; l < m; l++){
+        if (array[k,l] / 10 == 0)Console.Write($"0{array[k,l]} ");
+        else Console.Write($"{array[k,l]} ");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine();
